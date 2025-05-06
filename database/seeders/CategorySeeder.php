e88dceedae9822bdb6632795eb2a4730543a901c
dito2004123 <ditohermawan23@gmail.com>
2025-05-06 10:05:43 +0700

    membuat seeders dan faker

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,6 +12,38 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert([
+            [
+                'category_name' => 'Action',
+                'description' => 'Film dengan adegan-adegan penuh aksi dan ketegangan.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Comedy',
+                'description' => 'Film yang bertujuan untuk menghibur dan mengundang tawa.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Drama',
+                'description' => 'Film yang berfokus pada pengembangan karakter dan konflik emosional.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Sci-Fi',
+                'description' => 'Film dengan latar belakang ilmiah dan teknologi futuristik.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Romance',
+                'description' => 'Film yang berpusat pada kisah cinta dan hubungan romantis.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
+
